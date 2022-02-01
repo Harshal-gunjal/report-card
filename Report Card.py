1813d0769b783f0477c5a4613a6cffc89fc92b93
Harshal-gunjal <98735298+Harshal-gunjal@users.noreply.github.com>
2022-02-01 12:11:11 +0530

    Update Report Card.py

--- Report Card.py
+++ Report Card.py
@@ -7,8 +7,7 @@ print("First lets start with student's personal details.")
 print('\n')
 
 name = input("Enter the name of the student : ")
-class_ = input("Enter the class of the student : ")
-section = input("Enter the section of the student : ")
+class_ = input("Enter the class/sec of the student : ")
 roll_no = input("Enter the roll no. of the student : ")
 
 print('\n')
@@ -75,19 +74,31 @@ grade_5 = input("Enter the grade to be given ( best of 2 ) : ")
 totalmarks_5 = term_5 + pt_5 + assignment_5 + practical_5
 print('\n')
 
+average = ( totalmarks_1 + totalmarks_2 + totalmarks_3 + totalmarks_4 + totalmarks_5 ) / 5
 
 
 
-print('                                         REPORT CARD                                          ')
 
+print('                                      Atomic Energy Central School No. 3                              ')
+print('                                                 REPORT CARD                                          ')
+print('\n')
 
-table.field_names = ["SUBJECT", "TERM 1", "TERM 2", "PERIODIC TESTS", "ASSIGNMENT", "PRACTICLES", "TOTAL MARKS", "GRADE(Best of 2)"]
+print( "Name of the student : ", name )
+print( "Class/Sec : ", class_ )
+print( "Roll no. : ", roll_no )
+print('\n')
+
+table.field_names = ["SUBJECT", "TERM 1", "TERM 2", "PERIODIC TESTS", "ASSIGNMENT", "PRACTICALS", "TOTAL MARKS", "GRADE(Best of 2)"]
 
 table.add_row(["Maths", term1_1, term2_1, pt_1, assignment_1, practical_1, totalmarks_1, grade_1])
 table.add_row(["Physics", term1_2, term2_2, pt_2, assignment_2, practical_2, totalmarks_2, grade_2])
 table.add_row(["Chemistry", term1_3, term2_3, pt_3, assignment_3, practical_3, totalmarks_3, grade_3])
 table.add_row(["Computer / Bio", term1_4, term2_4, pt_4, assignment_4, practical_4, totalmarks_4, grade_4])
 table.add_row(["English", term1_5, term2_5, pt_5, assignment_5, practical_5, totalmarks_5, grade_5])
-
-
-print(table)
+table.add_row(["C.G.P.A. ", " ", " ", " = ", " ", " ", average, " "])
+table_line = table.get_string().split('\n')
+horizontal_line = table_line[0]
+result_lines = 1
+print("\n".join(table_line[:-(result_lines + 1)]))
+print(horizontal_line)
+print("\n".join(table_line[-(result_lines + 1):]))
